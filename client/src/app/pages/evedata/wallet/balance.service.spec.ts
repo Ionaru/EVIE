46616ca3fd714fa656fcca34fd191cf3efac8333
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2017-03-10 08:46:30 +0100

    Added more tests for the BalanceService (#22)
    And deleted old tsconfig.json

--- client/src/app/pages/evedata/wallet/balance.service.spec.ts
+++ client/src/app/pages/evedata/wallet/balance.service.spec.ts
@@ -1,9 +1,8 @@
 import { BaseRequestOptions, Http, Response, ResponseOptions, XHRBackend } from '@angular/http';
-import { async, getTestBed, TestBed } from '@angular/core/testing';
+import { getTestBed, TestBed } from '@angular/core/testing';
 import { MockBackend, MockConnection } from '@angular/http/testing';
 import { expect } from 'chai';
-import { stub, SinonStub } from 'sinon';
-// import * as sinon from 'sinon';
+import { assert, SinonFakeTimers, SinonStub, spy, stub, useFakeTimers } from 'sinon';
 
 import { Character } from '../../../components/character/character';
 import { EndpointService } from '../../../components/endpoint/endpoint.service';
@@ -11,21 +10,17 @@ import { Globals } from '../../../globals';
 import { BalanceService } from './balance.service';
 
 describe('Evedata', () => {
-
-  let cStub: SinonStub;
-
-  beforeEach(() => {
-    cStub = stub(console, 'error');
-  });
-
   describe('Wallet', () => {
     describe('BalanceService', () => {
 
+      let http: Http;
       let mockBackend: MockBackend;
       let balanceService: BalanceService;
       let globals: Globals;
+      let cStub: SinonStub;
+      let clock: SinonFakeTimers;
 
-      beforeEach(async(() => {
+      beforeEach(async () => {
         TestBed.configureTestingModule({
           providers: [
             BaseRequestOptions,
@@ -48,6 +43,7 @@ describe('Evedata', () => {
 
         const testbed = getTestBed();
         globals = testbed.get(Globals);
+
         const dummyData: ApiCharacterData = {
           characterId: 123,
           name: 'Dummy',
@@ -60,9 +56,20 @@ describe('Evedata', () => {
         };
         globals.selectedCharacter = new Character(dummyData);
 
+        http = testbed.get(Http);
         mockBackend = testbed.get(MockBackend);
         balanceService = testbed.get(BalanceService);
-      }));
+
+        cStub = stub(console, 'error');
+        localStorage.clear();
+      });
+
+      afterEach(() => {
+        cStub.restore();
+        if (clock) {
+          clock.restore();
+        }
+      });
 
       function setupConnections(backend: MockBackend, options: any) {
         backend.connections.subscribe((connection: MockConnection) => {
@@ -72,23 +79,40 @@ describe('Evedata', () => {
         });
       }
 
-      it('should be able to process balance data', async() => {
+      const fakeBalanceXML = `<?xml version='1.0' encoding='UTF-8'?>
+        <eveapi version="2">
+          <currentTime>2000-01-01 00:00:00</currentTime>
+          <result>
+            <rowset name="accounts" key="accountID" columns="accountID,accountKey,balance">
+            <row accountID="1234567" accountKey="1000" balance="500000.00" />
+            </rowset>
+          </result>
+          <cachedUntil>2000-01-01 00:15:00</cachedUntil>
+        </eveapi>`;
+
+      const fakeBalanceError = `<?xml version='1.0' encoding='UTF-8'?>
+        <eveapi version="2">
+          <currentTime>2000-01-01 00:00:00</currentTime>
+          <error code="224">DummyPermissionError</error>
+          <cachedUntil>2000-01-01 00:15:00</cachedUntil>
+        </eveapi>`;
+
+      const invalidXML = `<?xml version='1.0' encoding='UTF-8'?>
+        <eveapi version="2"
+          <currentTime>2000-01-01 00:00:00</currentTime>
+          <error code="224">DummyPermissionError</error>
+          cachedUntil>2000-01-01 00:15:00</cachedUntil>
+        </eveapi>`;
+
+      it('should be able to process balance data', async () => {
 
         setupConnections(mockBackend, {
-          body: `<?xml version='1.0' encoding='UTF-8'?>
-          <eveapi version="2">
-            <currentTime>2017-03-08 14:48:59</currentTime>
-            <result>
-              <rowset name="accounts" key="accountID" columns="accountID,accountKey,balance">
-              <row accountID="1234567" accountKey="1000" balance="500000.00" />
-              </rowset>
-            </result>
-            <cachedUntil>2017-03-08 14:59:20</cachedUntil>
-          </eveapi>`,
+          body: fakeBalanceXML,
           status: 200
         });
 
-        const balance = await balanceService.getBalance(true);
+        expect(localStorage.getItem('AccountBalance' + globals.selectedCharacter.characterId)).to.not.exist;
+        const balance = await balanceService.getBalance();
         expect(balance).to.be.a('string');
         expect(balance).to.equal('500000.00');
 
@@ -97,14 +121,65 @@ describe('Evedata', () => {
         expect(localStorage.getItem('AccountBalance' + globals.selectedCharacter.characterId)).to.exist;
       });
 
-      it('should be able to process an error response', async() => {
+      it('should get cached balance data from localStorage', async () => {
+
         setupConnections(mockBackend, {
-          body: `<?xml version='1.0' encoding='UTF-8'?>
-          <eveapi version="2">
-            <currentTime>2017-03-09 10:34:13</currentTime>
-            <error code="224">DummyPermissionError</error>
-            <cachedUntil>2017-03-10 10:34:13</cachedUntil>
-          </eveapi>`,
+          body: fakeBalanceXML,
+          status: 200
+        });
+
+        expect(localStorage.getItem('AccountBalance' + globals.selectedCharacter.characterId)).to.not.exist;
+        // First request to set up a cached result in localStorage
+        await balanceService.getBalance();
+
+        expect(localStorage.getItem('AccountBalance' + globals.selectedCharacter.characterId)).to.exist;
+
+        clock = useFakeTimers(new Date('2000-01-01 00:10:00').getTime());
+
+        // We watch 'http.get()' from this point on
+        const httpSpy = spy(http, 'get');
+        const balance = await balanceService.getBalance();
+        // 'http.get()' should not have been called, this means the result came from localStorage
+        assert.notCalled(httpSpy);
+
+        expect(balance).to.be.a('string');
+        expect(balance).to.equal('500000.00');
+        expect(Number(balance)).to.be.a('number');
+        expect(Number(balance)).to.equal(500000.00);
+        expect(localStorage.getItem('AccountBalance' + globals.selectedCharacter.characterId)).to.exist;
+      });
+
+      it('should fetch new balance data when cache is expired', async () => {
+
+        setupConnections(mockBackend, {
+          body: fakeBalanceXML,
+          status: 200
+        });
+
+        expect(localStorage.getItem('AccountBalance' + globals.selectedCharacter.characterId)).to.not.exist;
+        // First request to set up a cached result in localStorage
+        await balanceService.getBalance();
+
+        expect(localStorage.getItem('AccountBalance' + globals.selectedCharacter.characterId)).to.exist;
+
+        clock = useFakeTimers(new Date('2001-01-01 00:10:00').getTime());
+
+        // We watch 'http.get()' from this point on
+        const httpSpy = spy(http, 'get');
+        const balance = await balanceService.getBalance();
+        // 'http.get()' should been called, this means the result came from a new request
+        assert.calledOnce(httpSpy);
+
+        expect(balance).to.be.a('string');
+        expect(balance).to.equal('500000.00');
+        expect(Number(balance)).to.be.a('number');
+        expect(Number(balance)).to.equal(500000.00);
+        expect(localStorage.getItem('AccountBalance' + globals.selectedCharacter.characterId)).to.exist;
+      });
+
+      it('should be able to process an error response', async () => {
+        setupConnections(mockBackend, {
+          body: fakeBalanceError,
           status: 403
         });
 
@@ -114,8 +189,44 @@ describe('Evedata', () => {
         expect(localStorage.getItem('AccountBalance' + globals.selectedCharacter.characterId)).to.not.exist;
       });
 
-      it('should be able to process HTTP errors', async() => {
+      it('should be able to process an empty response', async () => {
         setupConnections(mockBackend, {});
+
+        const balance = await balanceService.getBalance(true);
+        expect(balance).to.be.a('string');
+        expect(balance).to.equal('Error');
+        expect(localStorage.getItem('AccountBalance' + globals.selectedCharacter.characterId)).to.not.exist;
+      });
+
+      it('should be able to process an invalid response', async () => {
+        setupConnections(mockBackend, {
+          body: `IamAnInvalidResponse`,
+          status: 200
+        });
+
+        const balance = await balanceService.getBalance(true);
+        expect(balance).to.be.a('string');
+        expect(balance).to.equal('Error');
+        expect(localStorage.getItem('AccountBalance' + globals.selectedCharacter.characterId)).to.not.exist;
+      });
+
+      it('should be able to process an invalid HTTP response', async () => {
+        setupConnections(mockBackend, {
+          what: 7,
+          problem: 'nothing'
+        });
+
+        const balance = await balanceService.getBalance(true);
+        expect(balance).to.be.a('string');
+        expect(balance).to.equal('Error');
+        expect(localStorage.getItem('AccountBalance' + globals.selectedCharacter.characterId)).to.not.exist;
+      });
+
+      it('should be able to process an invalid XML response', async () => {
+        setupConnections(mockBackend, {
+          body: invalidXML,
+          status: 200
+        });
 
         const balance = await balanceService.getBalance(true);
         expect(balance).to.be.a('string');
@@ -124,9 +235,4 @@ describe('Evedata', () => {
       });
     });
   });
-
-  afterEach(() => {
-    cStub.restore();
-  });
-
 });
