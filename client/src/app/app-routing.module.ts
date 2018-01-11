874e0b0223aea98be538f00ed59b0e5fe8826228
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2018-01-11 11:01:44 +0100

    Fix linting errors, gave routing guards their own files.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,58 +1,11 @@
-import { Injectable, NgModule } from '@angular/core';
-import { CanActivate, Resolve, Router, RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import 'rxjs/add/observable/of';
-import { Observable } from 'rxjs/Observable';
 
-import { Observer } from 'rxjs/Observer';
-import { AppReadyEvent } from './app-ready.event';
-import { UserService } from './models/user/user.service';
+import { AppReadyGuard } from './guards/app-ready.guard';
+import { AuthGuard } from './guards/auth.guard';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
-
-@Injectable()
-export class AppReadyGuard implements Resolve<boolean> {
-
-    /**
-     * Resolves if the app has started correctly.
-     */
-    public resolve(): Observable<any> {
-        if (AppReadyEvent.appReady) {
-            return Observable.of(true);
-        } else {
-            return AppReadyEvent.appReadyEvent;
-        }
-    }
-}
-
-@Injectable()
-export class AuthGuard implements CanActivate {
-
-    constructor(private router: Router) { }
-
-    public canActivate(): Observable<boolean> | boolean {
-        if (AppReadyEvent.appReady) {
-            if (UserService.user) {
-                return true;
-            } else {
-                this.router.navigate(['/']).then();
-                return false;
-            }
-        } else {
-            return Observable.create((observer: Observer<boolean>) => {
-                AppReadyEvent.appReadyEvent.subscribe(() => {
-                    if (UserService.user) {
-                        observer.next(true);
-                        observer.complete();
-                    } else {
-                        this.router.navigate(['/']).then();
-                        observer.next(false);
-                        observer.complete();
-                    }
-                });
-            });
-        }
-    }
-}
 
 const routes: Routes = [
     {path: '', component: HomeComponent, resolve: [AppReadyGuard]},
