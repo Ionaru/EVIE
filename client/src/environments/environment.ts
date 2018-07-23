90603a58d771465f49ae7513050d1bca37013a2e
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2018-07-23 10:22:49 +0200

    Remove unused legacy files, fixed spelling in a comment and updates.

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -11,6 +11,6 @@ export const environment = {
  * In development mode, to ignore zone related error stack frames such as
  * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
  * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
+ * because it will have performance impact when throwing an error.
  */
 import 'zone.js/dist/zone-error';  // Included with Angular CLI.
