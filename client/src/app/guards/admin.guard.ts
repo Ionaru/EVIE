c62f0bfd0227d0f449f808a688df8837dfd34130
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2018-10-04 13:35:43 +0200

    Add base guard and extend for auth and admin.

--- client/src/app/guards/admin.guard.ts
+++ client/src/app/guards/admin.guard.ts
@@ -1,40 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { Observable, Observer } from 'rxjs';
 
-import { AppReadyEventService } from '../app-ready-event.service';
 import { UserService } from '../models/user/user.service';
+import { BaseGuard } from './base.guard';
 
 @Injectable()
-export class AdminGuard implements CanActivate {
+export class AdminGuard extends BaseGuard {
 
-    private static checkCondition() {
+    public condition() {
         return UserService.user.isAdmin;
-    }
-
-    constructor(private router: Router) { }
-
-    public canActivate(): Observable<boolean> | boolean {
-        if (AppReadyEventService.appReady) {
-            if (AdminGuard.checkCondition()) {
-                return true;
-            } else {
-                this.router.navigate(['/']).then();
-                return false;
-            }
-        } else {
-            return new Observable((observer: Observer<boolean>) => {
-                AppReadyEventService.appReadyEvent.subscribe(() => {
-                    if (AdminGuard.checkCondition()) {
-                        observer.next(true);
-                        observer.complete();
-                    } else {
-                        this.router.navigate(['/']).then();
-                        observer.next(false);
-                        observer.complete();
-                    }
-                });
-            });
-        }
     }
 }
--- client/src/app/guards/auth.guard.ts
+++ client/src/app/guards/auth.guard.ts
@@ -1,40 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { Observable, Observer } from 'rxjs';
 
-import { AppReadyEventService } from '../app-ready-event.service';
 import { UserService } from '../models/user/user.service';
+import { BaseGuard } from './base.guard';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard extends BaseGuard {
 
-    private static checkCondition() {
-        return UserService.user;
-    }
-
-    constructor(private router: Router) { }
-
-    public canActivate(): Observable<boolean> | boolean {
-        if (AppReadyEventService.appReady) {
-            if (AuthGuard.checkCondition()) {
-                return true;
-            } else {
-                this.router.navigate(['/']).then();
-                return false;
-            }
-        } else {
-            return new Observable((observer: Observer<boolean>) => {
-                AppReadyEventService.appReadyEvent.subscribe(() => {
-                    if (AuthGuard.checkCondition()) {
-                        observer.next(true);
-                        observer.complete();
-                    } else {
-                        this.router.navigate(['/']).then();
-                        observer.next(false);
-                        observer.complete();
-                    }
-                });
-            });
-        }
+    public condition() {
+        return !!UserService.user;
     }
 }
