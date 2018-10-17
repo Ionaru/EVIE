4ef4d892125adf2807254b075171e1ff585d6ce6
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2018-10-18 00:21:38 +0200

    Fix for DST time issues on skills page, fixes #48

--- client/src/shared/calc.helper.ts
+++ client/src/shared/calc.helper.ts
@@ -2,5 +2,11 @@
  * Several static helper functions for calculations.
  */
 export class Calc {
+
+    public static maxIntegerValue = Math.pow(2, 31);
+
     public static partPercentage = (part: number, total: number) => (part / total) * 100;
+
+    public static wholeHoursLeft = (duration: number) => Math.floor(duration / (3600000));
+    public static wholeDaysLeft = (duration: number) => Math.floor(duration / (86400000));
 }
