7868139f05d1de5cee7df3d384adf0d94a4751a7
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2018-10-18 02:01:34 +0200

    Use sor-table in more places

--- client/src/app/pages/users/users.component.ts
+++ client/src/app/pages/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { IUsersResponse } from '../../../shared/interface.helper';
+import { ITableHeader } from '../../components/sor-table/sor-table.component';
 import { UsersService } from '../../data-services/users.service';
 
 @Component({
@@ -12,6 +13,32 @@ export class UsersComponent implements OnInit {
 
     public users: IUsersResponse[] = [];
 
+    public tableSettings: ITableHeader[] = [{
+        attribute: 'id',
+        sort: true,
+        title: 'ID',
+    }, {
+        attribute: 'username',
+        sort: true,
+    }, {
+        attribute: 'email',
+        sort: true,
+    }, {
+        attribute: 'characters.length',
+        sort: true,
+        title: 'Characters',
+    }, {
+        attribute: 'timesLogin',
+        sort: true,
+        title: 'Times logged in',
+    }, {
+        attribute: 'lastLogin',
+        pipe: 'date',
+        pipeVar: 'yyyy-MM-dd HH:mm:ss',
+        sort: true,
+        title: 'Last login',
+    }];
+
     constructor(private usersService: UsersService) { }
 
     public ngOnInit() {
@@ -21,5 +48,4 @@ export class UsersComponent implements OnInit {
             }
         });
     }
-
 }
