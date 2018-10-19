cc9630edc3322ffe4a254edc35419585b5622130
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2018-10-20 00:39:15 +0200

    Types caching

--- client/src/app/data-services/types.service.ts
+++ client/src/app/data-services/types.service.ts
@@ -8,12 +8,29 @@ import { BaseService } from './base.service';
 @Injectable()
 export class TypesService extends BaseService {
 
+    private typesCache: ITypesData[] = [];
+
     public async getTypes(...typeIds: number[]): Promise<ITypesData[] | undefined> {
+        const typesFromCache = this.typesCache.filter((type) => typeIds.includes(type.type_id));
+
+        if (typesFromCache.length === typeIds.length) {
+            return typesFromCache;
+        }
+
+        const cachedTypeIds = typesFromCache.map((type) => type.type_id);
+        const missingTypes = typeIds.filter((typeId) => !cachedTypeIds.includes(typeId));
+
         const url = 'data/types';
-        const response = await this.http.post<any>(url, typeIds).toPromise<IServerResponse<ITypesData[]>>().catch(this.catchHandler);
+        const response = await this.http.post<any>(url, missingTypes).toPromise<IServerResponse<ITypesData[]>>().catch(this.catchHandler);
         if (response instanceof HttpErrorResponse) {
             return undefined;
         }
+
+        if (response.data) {
+            this.typesCache.push(...response.data);
+            this.typesCache = Array.from(new Set(this.typesCache));
+        }
+
         return response.data;
     }
 
