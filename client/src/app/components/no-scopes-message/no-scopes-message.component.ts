f2d4fc0680ba720faf2aa5e763ef66e7ab98bf37
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2018-12-03 01:42:48 +0100

    Make the "missing scopes" message prettier and more useful.

--- client/src/app/components/no-scopes-message/no-scopes-message.component.ts
+++ client/src/app/components/no-scopes-message/no-scopes-message.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { faEyeSlash } from '@fortawesome/pro-regular-svg-icons';
 
 @Component({
   selector: 'app-no-scopes-message',
   styleUrls: ['./no-scopes-message.component.scss'],
   templateUrl: './no-scopes-message.component.html',
 })
-export class NoScopesMessageComponent { }
+export class NoScopesMessageComponent {
+    public noScopesIcon = faEyeSlash;
+}
