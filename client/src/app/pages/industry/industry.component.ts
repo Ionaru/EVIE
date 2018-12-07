e0ba4d7c46ff4fa5329a719b220d20ada91dc612
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2018-12-07 08:52:33 +0100

    Display cleaner industry data

--- client/src/app/pages/industry/industry.component.ts
+++ client/src/app/pages/industry/industry.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { faCopy, faHourglass, faMicroscope } from '@fortawesome/pro-regular-svg-icons';
+import { faGem, faIndustry } from '@fortawesome/pro-solid-svg-icons';
+import { Calc } from '../../../shared/calc.helper';
+import { Common } from '../../../shared/common.helper';
 
 import { IIndustryJobsData, IndustryActivity } from '../../../shared/interface.helper';
 import { IndustryJobsService } from '../../data-services/industry-jobs.service';
@@ -9,6 +13,11 @@ import { CharacterService } from '../../models/character/character.service';
 import { DataPageComponent } from '../data-page/data-page.component';
 import { ScopesComponent } from '../scopes/scopes.component';
 
+interface IExtendedIndustryJobsData extends IIndustryJobsData {
+    percentageDone?: number;
+    productName?: string;
+}
+
 @Component({
     selector: 'app-industry',
     styleUrls: ['./industry.component.scss'],
@@ -16,7 +25,14 @@ import { ScopesComponent } from '../scopes/scopes.component';
 })
 export class IndustryComponent extends DataPageComponent implements OnInit {
 
-    public industryJobs?: IIndustryJobsData[];
+    // Icons
+    public manufacturingIcon = faIndustry;
+    public copyIcon = faCopy;
+    public materialResearchIcon = faGem;
+    public timeResearchIcon = faHourglass;
+    public inventionIcon = faMicroscope;
+
+    public industryJobs?: IExtendedIndustryJobsData[];
     public IndustryActivity = IndustryActivity;
 
     constructor(private industryJobsService: IndustryJobsService, private typesService: TypesService,
@@ -36,8 +52,44 @@ export class IndustryComponent extends DataPageComponent implements OnInit {
 
     public async getIndustryJobs() {
         if (CharacterService.selectedCharacter && IndustryComponent.hasIndustryJobsScopes) {
-            const jobs = await this.industryJobsService.getIndustryJobs(CharacterService.selectedCharacter);
-            this.industryJobs = jobs;
+            this.industryJobs = await this.industryJobsService.getIndustryJobs(CharacterService.selectedCharacter);
         }
+
+        if (this.industryJobs) {
+
+            // Get ME / TE for BP
+
+            for (const job of this.industryJobs) {
+                const start = new Date(job.start_date).getTime();
+                const now = Date.now();
+
+                const timeElapsed = now - start;
+                job.percentageDone = Math.min(Calc.partPercentage((timeElapsed / 1000), job.duration), 100);
+            }
+
+            this.setProductNames(this.industryJobs);
+
+            Common.sortArrayByObjectProperty(this.industryJobs, 'job_id', false);
+            Common.sortArrayByObjectProperty(this.industryJobs, 'percentageDone', true);
+        }
+    }
+
+    public setProductNames(industryJobs: IExtendedIndustryJobsData[]) {
+        const namesToGet = industryJobs.map((job) => {
+            if (job.activity_id === IndustryActivity.manufacturing) {
+                return job.product_type_id;
+            }
+            return 0;
+        }).filter(Boolean);
+
+        this.namesService.getNames(...namesToGet).then(() => {
+            if (industryJobs) {
+                for (const job of industryJobs) {
+                    if (job.activity_id === IndustryActivity.manufacturing) {
+                        job.productName = NamesService.getNameFromData(job.product_type_id);
+                    }
+                }
+            }
+        });
     }
 }
