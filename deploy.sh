5e07cfbcba88c24afa3070ef9be723467b14a4b5
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2019-05-30 21:04:54 +0200

    Added travis origin header

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ url="http://85.25.237.10:6400/app/rest/buildQueue"
 data="<build><buildType id=\"${buildType}\"/></build>"
 authHeader="Authorization: Bearer ${apiKey}"
 contentHeader="Content-Type: application/xml"
-originHeader="Origin: http://85.25.237.10:6400"
+originHeader="Origin: https://travis-ci.org"
 
-curl -vvv -l --request POST ${url} --data "${data}" --header "${contentHeader}" --header "${authHeader}" --header ${originHeader}
+curl -vvv -l --request POST ${url} --data "${data}" --header "${contentHeader}" --header "${authHeader}" --header "${originHeader}"
