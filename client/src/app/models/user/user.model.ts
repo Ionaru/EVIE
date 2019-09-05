f0e455b97374a955f2ed4a3bc95526d4642a3ed5
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2019-09-05 15:02:19 +0200

    Removed unused login and register interfaces

--- client/src/app/models/user/user.model.ts
+++ client/src/app/models/user/user.model.ts
@@ -28,25 +28,10 @@ export interface ISSOLoginResponse {
     data: IUserApiData;
 }
 
-export interface ILoginResponse {
-    state: string;
-    message: string;
-    data?: IUserApiData;
-}
-
 export interface IUserApiData {
     username?: string;
     uuid: string;
     email?: string;
     isAdmin: boolean;
     characters: IApiCharacterData[];
-}
-
-export interface IRegisterResponse {
-    data: {
-        email_in_use: boolean;
-        username_in_use: boolean;
-    };
-    message: string;
-    state: 'error' | 'success';
 }
