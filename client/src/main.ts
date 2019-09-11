713a6854e0cc158281b9dc21441396e25ec147fe
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2019-09-11 15:07:54 +0200

    Completely remove polyfill configuration and older browser support

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
