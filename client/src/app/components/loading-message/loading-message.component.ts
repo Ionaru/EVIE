bd50caa51839e09d3fe017c45b845cb3bac9440f
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2019-11-07 18:01:25 +0100

    Switch to native bootstrap spinners instead of custom m-progress

--- client/src/app/components/loading-message/loading-message.component.ts
+++ client/src/app/components/loading-message/loading-message.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-loading-message',
-  styleUrls: ['./loading-message.component.scss'],
-  templateUrl: './loading-message.component.html',
+    selector: 'app-loading-message',
+    styleUrls: ['./loading-message.component.scss'],
+    templateUrl: './loading-message.component.html',
 })
-export class LoadingMessageComponent { }
+export class LoadingMessageComponent implements OnInit {
+
+    @Input()
+    public small = false;
+
+    @ViewChild('spinner', {
+        static: true,
+    })
+    public spinner?: ElementRef;
+
+    public ngOnInit(): void {
+        if (this.spinner && this.small) {
+            this.spinner.nativeElement.classList.add('spinner-border-sm');
+        }
+    }
+}
