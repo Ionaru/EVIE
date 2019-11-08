2d6106e50378113a7d51215dd731b41f2a569958
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2019-11-08 18:00:27 +0100

    Display some data

--- client/src/app/pages/mail/mail.component.ts
+++ client/src/app/pages/mail/mail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ICharacterMailsData } from '@ionaru/eve-utils';
 
 import { MailService } from '../../data-services/mail.service';
 import { CharacterService } from '../../models/character/character.service';
@@ -12,14 +13,17 @@ import { Scope } from '../scopes/scopes.component';
 })
 export class MailComponent extends DataPageComponent implements OnInit, OnDestroy {
 
+    public mails?: ICharacterMailsData;
+
     constructor(private mailService: MailService) {
         super();
         this.requiredScopes = [Scope.MAIL_READ];
     }
 
-    public ngOnInit() {
-        if (CharacterService.selectedCharacter) {
-            this.mailService.getMails(CharacterService.selectedCharacter).then();
+    public async ngOnInit() {
+        super.ngOnInit();
+        if (CharacterService.selectedCharacter && CharacterService.selectedCharacter.hasScope(Scope.MAIL_READ)) {
+            this.mails = await this.mailService.getMails(CharacterService.selectedCharacter).then();
         }
     }
 }
