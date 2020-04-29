ac63ea7edd3bac6717b2f4e571f09055039aff03
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2020-04-29 15:41:49 +0200

    Fix function not returning accurate result

--- client/src/shared/calc.helper.ts
+++ client/src/shared/calc.helper.ts
@@ -21,5 +21,5 @@ export class Calc {
     public static readonly wholeWeeks = (duration: number) => Math.floor(duration / Calc.week);
 
     public static readonly secondsToMilliseconds = (seconds: number) => seconds * 1000;
-    public static readonly millisecondsToSeconds = (milliseconds: number) => Math.floor(milliseconds / 1000);
+    public static readonly millisecondsToSeconds = (milliseconds: number) => milliseconds / 1000;
 }
