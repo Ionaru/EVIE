cbde50c9995d39f05f43eabd60293d57f9cc3ccc
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2020-05-06 02:11:12 +0200

    Add retry interceptor

---
+++ client/src/app/http-interceptors/esi-retry.interceptor.ts
@@ -0,0 +1,40 @@
+import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { EVE } from '@ionaru/eve-utils';
+
+import { Observable, throwError, timer } from 'rxjs';
+import { mergeMap, retryWhen } from 'rxjs/operators';
+
+export const genericRetryStrategy = () => (attempts: Observable<any>) => {
+
+    const scalingDuration = 1500;
+    const maxRetryAttempts = 2;
+
+    return attempts.pipe(
+        mergeMap((error, i) => {
+            const retryAttempt = i + 1;
+
+            if (retryAttempt > maxRetryAttempts || error.status < 500) {
+                return throwError(error);
+            }
+
+            return timer(retryAttempt * scalingDuration);
+        }),
+    );
+};
+
+@Injectable()
+export class EsiRetryInterceptor implements HttpInterceptor {
+
+    public intercept(request: HttpRequest<any>, next: HttpHandler) {
+
+        // We only want to retry ESI calls.
+        if (!request.url.includes(EVE.ESIURL)) {
+            return next.handle(request);
+        }
+
+        return next.handle(request).pipe(
+            retryWhen(genericRetryStrategy()),
+        );
+    }
+}
--- client/src/app/http-interceptors/index.ts
+++ client/src/app/http-interceptors/index.ts
@@ -2,6 +2,7 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { EsiCachingInterceptor } from './esi-caching.interceptor';
+import { EsiRetryInterceptor } from './esi-retry.interceptor';
 import { EsiUserAgentInterceptor } from './esi-user-agent.interceptor';
 import { EsiWarningInterceptor } from './esi-warning.interceptor';
 import { RequestCountInterceptor } from './request-count.interceptor';
@@ -14,4 +15,5 @@ export const httpInterceptorProviders = [
     {provide: HTTP_INTERCEPTORS, useClass: EsiUserAgentInterceptor, multi: true},
     {provide: HTTP_INTERCEPTORS, useClass: EsiWarningInterceptor, multi: true},
     {provide: HTTP_INTERCEPTORS, useClass: ServerTokenInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: EsiRetryInterceptor, multi: true},
 ];
