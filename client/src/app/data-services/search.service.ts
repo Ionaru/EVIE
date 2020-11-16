ae317e2ff35eabb30ed262937655c1038129cfad
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2020-11-16 11:16:11 +0100

    Add search caching

--- client/src/app/data-services/search.service.ts
+++ client/src/app/data-services/search.service.ts
@@ -6,14 +6,31 @@ import { BaseService, IServerResponse } from './base.service';
 
 export type SearchType = 'type' | 'region' | 'system' | 'constellation';
 
+interface ISearchCache {
+    [key: string]: IUniverseNamesDataUnit;
+}
+
 @Injectable()
 export class SearchService extends BaseService {
 
+    private static searchCache: ISearchCache = {};
+
     public async search(q: string, searchType: SearchType) {
+
+        const searchKey = `${searchType}:${q}`;
+        if (SearchService.searchCache[searchKey]) {
+            return SearchService.searchCache[searchKey];
+        }
+
         const response = await this.search$(q, searchType).toPromise().catch(this.catchHandler);
         if (response instanceof HttpErrorResponse) {
             return;
         }
+
+        if (response.data) {
+            SearchService.searchCache[searchKey] = response.data;
+        }
+
         return response.data;
     }
 
